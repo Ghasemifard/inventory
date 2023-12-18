c0a6ac2a5f066ed5e5ab0d2d97a4bbc1fa70914b
Ghasemifard <125352858+Ghasemifard@users.noreply.github.com>
2023-12-18 12:15:54 +0330

    Edit title of app

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ const NavBar = ({products}) => {
   
       <div className="h-12 flex items-center justify-center gap-x-4 bg-slate-700 mb-6">
         <h1 className="md:text-xl text-sm font-bold text-slate-300">
-          Inventory App usin Tailwind and Js
+          Inventory App usin React.js and Tailwind Css
         </h1>
         <span className="flex items-center justify-center w-7 h-7 bg-slate-500 rounded-full 
         border-2 border-slate-300 font-bold text-slate-300">
